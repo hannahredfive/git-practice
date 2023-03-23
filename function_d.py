8bdf603bf3ae452b4c3c8824f02d0f5fe15b3404
Hannah <hannah@chandleys.com>
2023-03-23 13:54:24 -0700

    Wrote initial implimentation of Max Value function.

--- function_d.py
+++ function_d.py
@@ -2,7 +2,14 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    counter = 0
+    for number in numbers:
+        if number > counter:
+            counter = number
+    
+    for number in numbers:
+        if number == counter:
+            return number
 
 
 if __name__ == "__main__":
